3f8b657060b809f4154f29f9b609ece0fb87a470
Peter Bryntesson <pbrynt@microsoft.com>
2021-03-09 16:29:45 +0100

    refactoring to BaseMonitoredItemModel and Data/EventMonitoredItemModel (#3)

---
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Models/EventMonitoredItemModel.cs
@@ -0,0 +1,59 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.OpcUa.Protocol.Models {
+    using Microsoft.Azure.IIoT.OpcUa.Core.Models;
+
+    /// <summary>
+    /// Data monitored item
+    /// </summary>
+    public class EventMonitoredItemModel : BaseMonitoredItemModel {
+        /// <summary>
+        /// Event filter
+        /// </summary>
+        public EventFilterModel EventFilter { get; set; }
+
+        /// <summary>
+        /// Clone
+        /// </summary>
+        /// <returns></returns>
+        public override BaseMonitoredItemModel Clone() {
+            return new EventMonitoredItemModel {
+                Id = Id,
+                TriggerId = TriggerId,
+                StartNodeId = StartNodeId,
+                SamplingInterval = SamplingInterval,
+                QueueSize = QueueSize,
+                DiscardNew = DiscardNew,
+                EventFilter = EventFilter.Clone(),
+                AttributeId = AttributeId,
+                IndexRange = IndexRange,
+                MonitoringMode = MonitoringMode,
+                DisplayName = DisplayName,
+                RelativePath = RelativePath,
+            };
+        }
+
+        /// <summary>
+        /// Compare items
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public override bool IsSameAs(BaseMonitoredItemModel other) {
+            var eventMonitoredItemModel = other as EventMonitoredItemModel;
+
+            if (eventMonitoredItemModel == null) {
+                return false;
+            }
+            if (!base.IsSameAs(other)) {
+                return false;
+            }
+            if (!EventFilter.IsSameAs(eventMonitoredItemModel.EventFilter)) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
